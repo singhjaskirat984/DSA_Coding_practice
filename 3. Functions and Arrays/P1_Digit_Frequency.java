5acbbeb3560846f22da89abc23010dc9f982d5e1
jaskirat singh <singhjaskirat984@gmail.com>
2021-07-31 19:42:12 +0530

    2 prog added
    digit freq and decimal to any base

---
+++ 3. Functions and Arrays/P1_Digit_Frequency.java
@@ -0,0 +1,39 @@
+/*
+Programme 1 - Digit Frequency 
+
+1. You are given a number n.
+2. You are given a digit d.
+3. You are required to calculate the frequency of digit d in number n.
+
+Sample Input
+994543234
+4
+
+Sample Output
+3
+ */
+
+import java.util.*;
+
+public class P1_Digit_Frequency {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int d = scn.nextInt();
+        int f = getDigitFrequency(n, d);
+        System.out.println(f);
+    }
+
+    public static int getDigitFrequency(int n, int d) {
+        // write code here
+        int count=0;
+        while(n!=0){
+            int rem = n%10; 
+            if(rem == d){
+                count++;
+            }
+            n=n/10;
+        }
+        return count;
+    }
+}
---
+++ 3. Functions and Arrays/P2_Decimal_To_Any_Base.java
@@ -0,0 +1,47 @@
+/*
+Programme 2 - Decimal To Any Base 
+
+1. You are given a decimal number n.
+2. You are given a base b.
+3. You are required to convert the number n into its corresponding value in base b.
+
+Input Format
+A number n
+A base b
+
+Output Format
+A number representing corresponding value of n in number system of base b
+
+Sample Input
+57
+2
+
+Sample Output
+111001
+
+ */
+
+
+import java.util.*;
+
+public class P2_Decimal_To_Any_Base {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int b = scn.nextInt();
+        int dn = getValueInBase(n, b);
+        System.out.println(dn);
+    }
+
+    public static int getValueInBase(int n, int b){
+        int power=1;
+        int ans=0;
+        while(n>0){
+            int rem = n%b;
+            n=n/b;
+            ans = ans + (power*rem);
+            power = power*10;
+        }
+        return ans;
+    }
+}
